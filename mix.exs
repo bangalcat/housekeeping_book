87ad83ed5d30dcbf347c4535fd2741b7acb881a7
Donghyun <semaj9310@gmail.com>
2023-11-16 21:49:47 +0900

    add boundary to deps
    Signed-off-by: Donghyun <semaj9310@gmail.com>

--- mix.exs
+++ mix.exs
@@ -50,7 +50,8 @@ defmodule HousekeepingBook.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:boundary, "~> 0.10"}
     ]
   end
 
