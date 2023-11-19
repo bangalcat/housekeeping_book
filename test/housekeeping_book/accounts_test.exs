6eb7e2c94c5eaddbe5ffad713c09a3fa68ba1ba0
Donghyun <semaj9310@gmail.com>
2023-11-19 10:03:00 +0900

    update tests to using factory
    Signed-off-by: Donghyun <semaj9310@gmail.com>

--- test/housekeeping_book/accounts_test.exs
+++ test/housekeeping_book/accounts_test.exs
@@ -1,30 +1,30 @@
 defmodule HousekeepingBook.AccountsTest do
   use HousekeepingBook.DataCase
 
-  import HousekeepingBook.AccountsFixtures
-
   alias HousekeepingBook.Accounts
   alias HousekeepingBook.Schema.User
+
+  @moduletag :current
 
   describe "users" do
     @invalid_attrs %{name: nil, email: nil}
 
     test "list_users/0 returns all users" do
-      user = user_fixture()
+      user = insert!(:user)
       assert Accounts.list_users() == [user]
     end
 
     test "get_user!/1 returns the user with given id" do
-      user = user_fixture()
+      user = insert!(:user)
       assert Accounts.get_user!(user.id) == user
     end
 
     test "create_user/1 with valid data creates a user" do
-      valid_attrs = %{name: "some name", email: "some email"}
+      valid_attrs = %{name: "some name", email: "some@email.com", type: :normal}
 
       assert {:ok, %User{} = user} = Accounts.create_user(valid_attrs)
       assert user.name == "some name"
-      assert user.email == "some email"
+      assert user.email == "some@email.com"
     end
 
     test "create_user/1 with invalid data returns error changeset" do
@@ -32,28 +32,28 @@ defmodule HousekeepingBook.AccountsTest do
     end
 
     test "update_user/2 with valid data updates the user" do
-      user = user_fixture()
-      update_attrs = %{name: "some updated name", email: "some updated email"}
+      user = insert!(:user)
+      update_attrs = %{name: "some updated name", email: "some@email.com"}
 
       assert {:ok, %User{} = user} = Accounts.update_user(user, update_attrs)
       assert user.name == "some updated name"
-      assert user.email == "some updated email"
+      assert user.email == "some@email.com"
     end
 
     test "update_user/2 with invalid data returns error changeset" do
-      user = user_fixture()
+      user = insert!(:user)
       assert {:error, %Ecto.Changeset{}} = Accounts.update_user(user, @invalid_attrs)
       assert user == Accounts.get_user!(user.id)
     end
 
     test "delete_user/1 deletes the user" do
-      user = user_fixture()
+      user = insert!(:user)
       assert {:ok, %User{}} = Accounts.delete_user(user)
       assert_raise Ecto.NoResultsError, fn -> Accounts.get_user!(user.id) end
     end
 
     test "change_user/1 returns a user changeset" do
-      user = user_fixture()
+      user = insert!(:user)
       assert %Ecto.Changeset{} = Accounts.change_user(user)
     end
   end
--- test/housekeeping_book/records_test.exs
+++ test/housekeeping_book/records_test.exs
@@ -1,22 +1,40 @@
 defmodule HousekeepingBook.RecordsTest do
   use HousekeepingBook.DataCase
 
-  import HousekeepingBook.RecordsFixtures
-
   alias HousekeepingBook.Records
   alias HousekeepingBook.Schema.Record
+
+  @moduletag :current
+
+  describe "list_records" do
+    setup [:setup_records]
+
+    test "list_records/0 returns all records", %{records: records} do
+      result_records = Records.list_records()
+
+      for {expect, result} <- Enum.zip(records, result_records) do
+        assert_same_schema(expect, result)
+      end
+    end
+
+    test "list_records/1 with pagination options returns paginated records ", %{
+      records: expect_records
+    } do
+      options = %{page: 1, per_page: 5}
+      result_records = Records.list_records(options)
+
+      for {expect, result} <- expect_records |> Enum.take(5) |> Enum.zip(result_records) do
+        assert_same_schema(expect, result)
+      end
+    end
+  end
 
   describe "records" do
     @invalid_attrs %{date: nil, description: nil, amount: nil}
 
-    test "list_records/0 returns all records" do
-      record = record_fixture()
-      assert Records.list_records() == [record]
-    end
-
     test "get_record!/1 returns the record with given id" do
-      record = record_fixture()
-      assert Records.get_record!(record.id) == record
+      record = insert!(:record)
+      assert_same_schema(Records.get_record!(record.id), record)
     end
 
     test "create_record/1 with valid data creates a record" do
@@ -33,7 +51,7 @@ defmodule HousekeepingBook.RecordsTest do
     end
 
     test "update_record/2 with valid data updates the record" do
-      record = record_fixture()
+      record = insert!(:record)
 
       update_attrs = %{
         date: ~U[2023-11-16 05:48:00Z],
@@ -48,20 +66,29 @@ defmodule HousekeepingBook.RecordsTest do
     end
 
     test "update_record/2 with invalid data returns error changeset" do
-      record = record_fixture()
+      record = insert!(:record)
       assert {:error, %Ecto.Changeset{}} = Records.update_record(record, @invalid_attrs)
       assert record == Records.get_record!(record.id)
     end
 
     test "delete_record/1 deletes the record" do
-      record = record_fixture()
+      record = insert!(:record)
       assert {:ok, %Record{}} = Records.delete_record(record)
       assert_raise Ecto.NoResultsError, fn -> Records.get_record!(record.id) end
     end
 
     test "change_record/1 returns a record changeset" do
-      record = record_fixture()
+      record = insert!(:record)
       assert %Ecto.Changeset{} = Records.change_record(record)
     end
+  end
+
+  def setup_records(_) do
+    records =
+      for _ <- 1..10 do
+        insert!(:record, %{})
+      end
+
+    {:ok, [records: records]}
   end
 end
