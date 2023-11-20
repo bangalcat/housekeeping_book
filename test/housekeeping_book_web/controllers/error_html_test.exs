e6088eaae540ca436e1824a268662c36f05661ab
Donghyun <semaj9310@gmail.com>
2023-11-20 10:46:19 +0900

    chore: formatting
    Signed-off-by: Donghyun <semaj9310@gmail.com>

--- test/housekeeping_book_web/controllers/error_html_test.exs
+++ test/housekeeping_book_web/controllers/error_html_test.exs
@@ -9,6 +9,7 @@ defmodule HousekeepingBookWeb.ErrorHTMLTest do
   end
 
   test "renders 500.html" do
-    assert render_to_string(HousekeepingBookWeb.ErrorHTML, "500", "html", []) == "Internal Server Error"
+    assert render_to_string(HousekeepingBookWeb.ErrorHTML, "500", "html", []) ==
+             "Internal Server Error"
   end
 end
