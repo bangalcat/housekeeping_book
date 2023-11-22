079280905ad8341adaf9090ad51fb1f8cd69d613
Donghyun <semaj9310@gmail.com>
2023-11-22 16:20:04 +0900

    introduce bandit adapter
    Signed-off-by: Donghyun <semaj9310@gmail.com>

--- mix.exs
+++ mix.exs
@@ -42,6 +42,7 @@ defmodule HousekeepingBook.MixProject do
   defp deps do
     [
       {:bcrypt_elixir, "~> 3.0"},
+      {:bandit, "~> 1.0"},
       {:phoenix, "~> 1.7.10"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.10"},
