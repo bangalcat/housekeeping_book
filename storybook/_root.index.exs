4ecd3eda847ac1c865e66e26477aed1cc4b35e59
Donghyun <semaj9310@gmail.com>
2023-11-23 19:15:02 +0900

    dep: introduce phoenix storybook
    Signed-off-by: Donghyun <semaj9310@gmail.com>

---
+++ storybook/_root.index.exs
@@ -0,0 +1,17 @@
+defmodule Storybook.Root do
+  use Boundary, check: [in: false, out: false]
+  # See https://hexdocs.pm/phoenix_storybook/PhoenixStorybook.Index.html for full index
+  # documentation.
+
+  use PhoenixStorybook.Index
+
+  def folder_icon, do: {:fa, "book-open", :light, "lsb-mr-1"}
+  def folder_name, do: "Storybook"
+
+  def entry("welcome") do
+    [
+      name: "Welcome Page",
+      icon: {:fa, "hand-wave", :thin}
+    ]
+  end
+end
