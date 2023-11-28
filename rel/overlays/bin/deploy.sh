d580c9f9cc2f46bcd4e97dcc4c965a59a7191f4d
Donghyun <semaj9310@gmail.com>
2023-11-28 13:28:11 +0900

    seriously
    Signed-off-by: Donghyun <semaj9310@gmail.com>

--- rel/overlays/bin/deploy.sh
+++ rel/overlays/bin/deploy.sh
@@ -19,8 +19,8 @@ echo 'Assets deploy..'
 mix assets.deploy
 current_release=$(ls ../releases | sort -nr | head -n 1)
 now_in_unix_seconds=$(date +'%s')
-if [[ $current_release == '']]; then 
-  current_release=$now_in_unix_seconds
+if [[ $current_release == '' ]]; then
+	current_release=$now_in_unix_seconds
 fi
 
 echo 'Current Release: ' $current_release
@@ -30,29 +30,28 @@ mix release --path ../releases/${now_in_unix_seconds}
 
 source ../releases/${current_release}/releases/0.1.0/env.sh
 
-if [[ $HTTP_PORT == '4000']]; then
-  http=4001
-  https=4041
-  old_port=4000
+if [[ $HTTP_PORT == '4000' ]]; then
+	http=4001
+	https=4041
+	old_port=4000
 else
-  http=4000
-  https=4040
-  old_port=4001
+	http=4000
+	https=4040
+	old_port=4001
 fi
 
 # Put env vars with the ports to forward to, and set non-conflicting node name
-echo "export HTTP_PORT=${http}" >> ../releases/${now_in_unix_seconds}/releases/0.1.0/env.sh
-echo "export HTTPS_PORT=${https}" >> ../releases/${now_in_unix_seconds}/releases/0.1.0/env.sh
-echo "export RELEASE_NAME=${http}" >> ../releases/${now_in_unix_seconds}/releases/0.1.0/env.sh
-echo 'export RELEASE_DISTRIBUTION="name"' >> ../releases/${now_in_unix_seconds}/releases/0.1.0/env.sh
-echo "export RELEASE_NODE=bangl${http}@0.0.0.0" >> ../releases/${now_in_unix_seconds}/releases/0.1.0/env.sh
-awk '/^[^#]/ {print "export " $0}' .env >> ../releases/${now_in_unix_seconds}/releases/0.1.0/env.sh
-
+echo "export HTTP_PORT=${http}" >>../releases/${now_in_unix_seconds}/releases/0.1.0/env.sh
+echo "export HTTPS_PORT=${https}" >>../releases/${now_in_unix_seconds}/releases/0.1.0/env.sh
+echo "export RELEASE_NAME=${http}" >>../releases/${now_in_unix_seconds}/releases/0.1.0/env.sh
+echo 'export RELEASE_DISTRIBUTION="name"' >>../releases/${now_in_unix_seconds}/releases/0.1.0/env.sh
+echo "export RELEASE_NODE=bangl${http}@0.0.0.0" >>../releases/${now_in_unix_seconds}/releases/0.1.0/env.sh
+awk '/^[^#]/ {print "export " $0}' .env >>../releases/${now_in_unix_seconds}/releases/0.1.0/env.sh
 
 # Set the release to the new version
 rm ../env_vars || true
 touch ../env_vars
-echo "RELEASE=${now_in_unix_seconds}" >> ../env_vars
+echo "RELEASE=${now_in_unix_seconds}" >>../env_vars
 
 # Run migrations
 mix ecto.migrate
@@ -60,9 +59,9 @@ mix ecto.migrate
 # Boot the new version of the app
 sudo systemctl start my_app@${http}
 # Wait for the new version to boot
-until $(curl --output /dev/null --silent --head --fail   localhost:${http}); do
-  echo 'Waiting for app to boot...'
-  sleep 1
+until $(curl --output /dev/null --silent --head --fail localhost:${http}); do
+	echo 'Waiting for app to boot...'
+	sleep 1
 done
 
 # Switch forwarding of ports 443 and 80 to the ones the new app is listening on
