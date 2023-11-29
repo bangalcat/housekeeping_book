84e7bfbe638438c82763c0b0e3831981c3eca57a
Donghyun <semaj9310@gmail.com>
2023-11-30 01:33:11 +0900

    update scroll js
    Signed-off-by: Donghyun <semaj9310@gmail.com>

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,12 +1,15 @@
-function scrollToElement(id, container) {
+function scrollToElement(id, container, is_window = false) {
   let element = document.getElementById(id);
   if (!element || !container) return;
-  container.scrollTo({ top: element.offsetTop, behavior: "smooth" });
+  if (is_window)
+    document.getElementById("main-container").scrollTo({ top: element.offsetTop, behavior: "smooth" });
+  else
+    container.scrollTo({ top: element.offsetTop, behavior: "smooth" });
 }
 let Scrolling = {
   mounted() {
-    this.handleEvent("scroll_to", ({ id }) => {
-      scrollToElement(id, this.el);
+    this.handleEvent("scroll_to", ({ id, is_window }) => {
+      scrollToElement(id, this.el, is_window);
     })
   },
 }
