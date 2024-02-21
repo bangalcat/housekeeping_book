8acd49fd3f2583a62741ee44004edd396c4a2fd3
Donghyun <semaj9310@gmail.com>
2024-02-21 16:56:20 +0900

    chore: add spark formatter
    Signed-off-by: Donghyun <semaj9310@gmail.com>

--- .formatter.exs
+++ .formatter.exs
@@ -1,7 +1,7 @@
 [
-  import_deps: [:ecto, :ecto_sql, :phoenix, :ash, :ash_postgres],
+  import_deps: [:ecto, :ecto_sql, :phoenix, :ash, :ash_postgres, :ash_authentication],
   subdirectories: ["priv/*/migrations"],
-  plugins: [Phoenix.LiveView.HTMLFormatter],
+  plugins: [Phoenix.LiveView.HTMLFormatter, Spark.Formatter],
   inputs: [
     "*.{heex,ex,exs}",
     "{config,lib,test}/**/*.{heex,ex,exs}",
