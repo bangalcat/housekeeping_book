c01e73b13e13dd9bf02f08f12892305e49dd724e
Donghyun <semaj9310@gmail.com>
2024-10-27 15:55:14 +0900

    fix confirm email delivery
    Signed-off-by: Donghyun <semaj9310@gmail.com>

--- lib/housekeeping_book/accounts/user/senders/send_new_user_confirmation_email.ex
+++ lib/housekeeping_book/accounts/user/senders/send_new_user_confirmation_email.ex
@@ -3,7 +3,7 @@ defmodule HousekeepingBook.Accounts.User.Senders.SendNewUserConfirmationEmail do
 
   @impl true
   def send(user, token, _opts) do
-    url = HousekeepingBook.Accounts.web_paths().new_user_confirm_url(token)
+    url = HousekeepingBook.Accounts.web_paths().new_user_confirmation_path(token)
 
     HousekeepingBook.Accounts.UserNotifier.deliver_confirmation_instructions(
       user,
