df202395a33756ad0a642543268cf067e525bdba
Donghyun <semaj9310@gmail.com>
2024-10-27 15:36:26 +0900

    add user confirmation add on
    Signed-off-by: Donghyun <semaj9310@gmail.com>

---
+++ lib/housekeeping_book/accounts/user/senders/send_new_user_confirmation_email.ex
@@ -0,0 +1,13 @@
+defmodule HousekeepingBook.Accounts.User.Senders.SendNewUserConfirmationEmail do
+  use AshAuthentication.Sender
+
+  @impl true
+  def send(user, token, _opts) do
+    url = HousekeepingBook.Accounts.web_paths().new_user_confirm_url(token)
+
+    HousekeepingBook.Accounts.UserNotifier.deliver_confirmation_instructions(
+      user,
+      url
+    )
+  end
+end
--- lib/housekeeping_book/accounts/web_paths.ex
+++ lib/housekeeping_book/accounts/web_paths.ex
@@ -1,3 +1,4 @@
 defmodule HousekeepingBook.Accounts.WebPaths do
   @callback user_reset_password_path(token :: String.t()) :: String.t()
+  @callback new_user_confirmation_path(token :: String.t()) :: String.t()
 end
